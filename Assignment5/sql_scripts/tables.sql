c23f359352dc3eb70536f91edc31cd868ab3a579
Benzo <123708293+atong4@users.noreply.github.com>
2023-03-14 22:56:09 -0400

    Delete Assignment5 directory

--- Assignment5/sql_scripts/tables.sql
+++
@@ -1,9 +0,0 @@
--- resources Table:
-CREATE TABLE IF NOT EXISTS resources (
-    id int NOT NULL AUTO_INCREMENT,
-    item varchar(50) NOT NULL,
-    amount int NOT NULL,
-    PRIMARY KEY (id)
-);
-
--- sandwiches table:
